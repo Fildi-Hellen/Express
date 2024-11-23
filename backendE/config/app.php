13b97c6a4c740f44940f638f16607db91b139944
Fildi-Hellen <141173418+Fildi-Hellen@users.noreply.github.com>
2024-11-23 11:52:21 +0100

    news

--- backendE/config/app.php
+++ backendE/config/app.php
@@ -127,9 +127,5 @@ return [
     // Other providers...
     App\Providers\AppServiceProvider::class,
 ],
-$app = new Illuminate\Foundation\Application(
-    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
-)
-
 
 ];
