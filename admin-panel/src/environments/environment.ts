450d45dd37005814f3c7db6e8787dd05b2b53060
Fildi-Hellen <141173418+Fildi-Hellen@users.noreply.github.com>
2025-08-28 16:03:14 +0100

    hope it works

--- admin-panel/src/environments/environment.ts
+++ admin-panel/src/environments/environment.ts
@@ -1,8 +1,8 @@
 
-// export const environment = {
+export const environment = {
   
-//   // googleMapsApiKey:'AIzaSyBWp5W006BrypJCT8hWRsQkwv3QLVIf5ME'
-//  apiBase: 'http://127.0.0.1:8000/api',
-//   wsUrl: 'ws://localhost:8080' // WebSocket URL for real-time messaging
-// };
+  // googleMapsApiKey:'AIzaSyBWp5W006BrypJCT8hWRsQkwv3QLVIf5ME'
+ apiBase: 'http://127.0.0.1:8000/api',
+  wsUrl: 'ws://localhost:8080' // WebSocket URL for real-time messaging
+};
 
