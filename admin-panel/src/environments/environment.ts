68f02c9e52dc1a9bf53cb361753bb950aacfca5f
Fildi-Hellen <141173418+Fildi-Hellen@users.noreply.github.com>
2025-09-11 10:22:37 +0100

    new

--- admin-panel/src/environments/environment.ts
+++ admin-panel/src/environments/environment.ts
@@ -1,8 +1,5 @@
-
 export const environment = {
-  
-  // googleMapsApiKey:'AIzaSyBWp5W006BrypJCT8hWRsQkwv3QLVIf5ME'
- apiBase: 'http://127.0.0.1:8000/api',
-  wsUrl: 'ws://localhost:8080' // WebSocket URL for real-time messaging
+  production: false,
+  apiBase: 'http://127.0.0.1:8000/api',
+  wsUrl: 'ws://localhost:8080',
 };
-
